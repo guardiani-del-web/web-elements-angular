05c6f6570e3b75e7da14bc8a40e59a39d3372e77
Gabriel <g.mayta.trigozo@accenture.com>
2020-06-09 02:14:07 +0200

    add radio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,20 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./app.component.html"
 })
 export class AppComponent implements OnInit {
+  fruits: Array<{ value: string, label: string }>;
   countries: Array<{ value: string, label: string }>;
 
   ngOnInit() {
+    this.fruits = [
+      { label: 'Banana', value: 'banana' },
+      { label: 'Pear', value: 'pear' },
+      { label: 'Apple', value: 'apple' }
+    ];
     this.countries = [
       { label: 'Italy', value: 'it' },
       { label: 'Peru', value: 'pe' },
       { label: 'Germany', value: 'de' }
-    ]
+    ];
   }
 
   checkboxHandler(currentSelection: []) {
